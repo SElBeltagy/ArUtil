7420c2ed49b48070a6de976ca796d0155f9f2ce7
Samhaa R. El-Beltagy <45735030+SElBeltagy@users.noreply.github.com>
2018-12-13 16:18:40 +0200

    Add files via upload

---
+++ ArUtils.py
@@ -0,0 +1,106 @@
+import re
+import codecs
+
+def normalizeText(text):
+    """
+        normalizes all forms to alf to ا, converts ة to ه, and ى to ي.  It also converts new lines and tabs to a single space  
+        and seperates common punctuation marks from text 
+    """	
+
+
+    search = ["أ", "إ", "آ", "ة", "_", "-", "/", ".", "،", " و ", " يا ", '"', "ـ", "'", "ى", "\\", '\n', '\t','&quot;', '?', '؟', '!']
+    replace = ["ا", "ا", "ا", "ه", " ", " ", "", "", "", " و", " يا", "", "", "", "ي", "", ' ', ' ', ' ', ' ? ', ' ؟ ',  ' ! ']
+
+    #search = ["آ", "إ", "أ", "ة"]
+    #replace = ["ا", "ا", "ا", "ه"]
+
+    for i in range(0, len(search)):
+        text = text.replace(search[i], replace[i])
+    return text
+
+def removeTashkeel(text): 
+    # Removes Tashkeel from input text
+
+    
+    p_tashkeel = re.compile(r'[\u0616-\u061A\u064B-\u0652\u06D6-\u06ED\u08F0-\u08F3\uFC5E-\uFC63\u0670]')
+    text = re.sub(p_tashkeel,"", text)
+    return text
+
+
+def getCount(d, key):
+    c = d.get(key)
+    if c == None:
+        return 0
+    return c
+
+
+def genNgramsInBuckets(txt, grams):
+    #Takes as input some text represented as a list, and generates all possible ngrams based on the grams given as a parameter. 
+    #The function returns a list of dictionaries of size 'grams'. The first entry represents the list of all uni-grams and their counts, 
+    #The secong, all uni-grams and their counts, and so on. 
+    #This works on any text, not just Arabic.  For example given the following code:
+    #     x ="test to test the function".split()
+    #     v =ArUtils.genNgramsInBuckets(x,3)
+    #The output will be:
+    #     [{'test': 2, 'to': 1, 'the': 1, 'function': 1}, {'test to': 1, 'to test': 1, 'test the': 1, 'the function': 1},
+    #      {'test to test': 1, 'to test the': 1, 'test the function': 1}]
+
+
+    result = []
+    for x in range(grams):
+        result.append({})
+    if grams < 1:
+        return
+
+    cnt = 0
+    j = 0
+    for i in txt:
+        result[0][i] = getCount(result[0], i) + 1
+        y = 1
+        # print(y,cnt,grams)
+        while y <= cnt and y < grams:
+            np = []
+            for u in range(y):
+                np.append(txt[j - y + u])
+            np.append(i)
+            # np = tuple(np)
+            np = " ".join(np)
+            # print( np)
+            result[y][np] = getCount(result[y], np) + 1
+            y = y + 1
+        j = j + 1
+        cnt = cnt + 1
+    return result 
+
+def genNgrams(txt, grams):
+    result = {}
+
+    cnt = 0
+    j = 0
+    for i in txt:
+        result[i] = getCount(result, i) + 1
+        y = 1
+        # print(y,cnt,grams)
+        while y <= cnt and y < grams:
+            np = []
+            for u in range(y):
+                np.append(txt[j - y + u])
+            np.append(i)
+            # np = tuple(np)
+            np = " ".join(np)
+            # print( np)
+            result[np] = getCount(result, np) + 1
+            y = y + 1
+        j = j + 1
+        cnt = cnt + 1
+    return result 
+
+def getContents(fname, normalize=True, removeTaskeel=True):
+    f = codecs.open(fname, "r", "utf-8")
+    #f = open(fname, "r")
+    contents  = f.read()
+    if normalize:
+        contents = normalizeText(contents)
+    if removeTaskeel:
+        contents = removeTashkeel(contents)
+    return contents
